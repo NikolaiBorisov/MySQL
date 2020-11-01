b4455e309dcba24cb07f94308f03323e98dc221c
NIKOLAI <borisov_n_88@mail.ru>
2020-11-01 17:03:18 +0300

    2nd_HOMETASK_UPDATED

--- 2l_regions.sql
+++ 2l_regions.sql
@@ -1,9 +1,29 @@
-CREATE TABLE `_regions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `country_id` int DEFAULT NULL,
-  `title` varchar(150) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_country_id_idx` (`country_id`),
-  KEY `regions_title` (`title`),
-  CONSTRAINT `fk_country_region_id` FOREIGN KEY (`country_id`) REFERENCES `_countries` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5468685 DEFAULT CHARSET=utf8;
+ALTER TABLE `geodata`.`_regions` 
+DROP COLUMN `title_cz`,
+DROP COLUMN `title_lv`,
+DROP COLUMN `title_lt`,
+DROP COLUMN `title_ja`,
+DROP COLUMN `title_pl`,
+DROP COLUMN `title_it`,
+DROP COLUMN `title_fr`,
+DROP COLUMN `title_de`,
+DROP COLUMN `title_pt`,
+DROP COLUMN `title_es`,
+DROP COLUMN `title_en`,
+DROP COLUMN `title_be`,
+DROP COLUMN `title_ua`;
+ALTER TABLE `geodata`.`_regions` 
+CHANGE COLUMN `region_id` `id` INT NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN `title_ru` `title` VARCHAR(150) NOT NULL ,
+ADD PRIMARY KEY (`id`),
+ADD INDEX `fk_countryid_regions_idx` (`country_id` ASC) VISIBLE;
+;
+ALTER TABLE `geodata`.`_regions` 
+ADD CONSTRAINT `fk_countryid_regions`
+  FOREIGN KEY (`country_id`)
+  REFERENCES `geodata`.`_countries` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+ALTER TABLE `geodata`.`_regions` 
+ADD INDEX `index1` (`title` ASC) VISIBLE;
+;
